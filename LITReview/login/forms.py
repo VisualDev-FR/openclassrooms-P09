65afc7e353cdfcb7f908005d94d78df1e50c8f15
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-02-24 13:54:39 +0100

    Cosmétique des pages de login

--- LITReview/login/forms.py
+++ LITReview/login/forms.py
@@ -3,6 +3,11 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
+
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':"Nom d'utilisateur"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':"Mot de passe"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':"Confirmer mot de passe"}))
+   
     class Meta:
         model = User
         fields = [
@@ -13,9 +18,13 @@ class RegisterForm(UserCreationForm):
 
 
 class LoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':"Nom d'utilisateur"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':"Mot de passe"}))
+
     class Meta:
         model = User
         fields = [
             'username',
             'password',
-        ]    
+        ]
