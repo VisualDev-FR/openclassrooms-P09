82fc37dd3b705a40244509c2c6a763896c22f6bc
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-03-05 20:06:36 +0100

    mise en place des requetes de flux complètes

--- LITReview/feed/views.py
+++ LITReview/feed/views.py
@@ -3,26 +3,46 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from django.contrib.auth import logout
 from django.db.models import CharField, Value
-from LITReview.models import Ticket, Review
+from LITReview.models import Ticket, Review, UserFollows
+from django.contrib.auth.models import User
 from itertools import chain
+import typing
+
+
+def get_viewable_users(user: User) -> typing.List[User]:
+
+    viewable_users = [user]
+    viewable_users.extend(
+        [userfollow.followed_user for userfollow in UserFollows.objects.filter(user=user)]
+    )
+
+    return viewable_users
 
 
 def get_users_viewable_reviews(request: HttpRequest):
-    return Review.objects.filter(user=request.user.pk).annotate(content_type=Value('REVIEW', CharField()))
+
+    viewable_users = get_viewable_users(request.user)   # type: ignore
+    viewable_reviews = Review.objects.filter(user__in=viewable_users)
+
+    return viewable_reviews.annotate(content_type=Value('REVIEW', CharField()))
 
 
 def get_users_viewable_tickets(request: HttpRequest):
-    return Ticket.objects.filter(user=request.user.pk).annotate(content_type=Value('TICKET', CharField()))
+
+    viewable_users = get_viewable_users(request.user)   # type: ignore
+    viewable_tickets = Ticket.objects.filter(user__in=viewable_users)
+
+    return viewable_tickets.annotate(content_type=Value('TICKET', CharField()))
 
 
 @login_required
 def feed(request: HttpRequest):
 
     # returns annotated queryset of reviews
-    reviews = get_users_viewable_reviews(request)
+    reviews = set(get_users_viewable_reviews(request))
 
     # returns annotated queryset of tickets
-    tickets = get_users_viewable_tickets(request)
+    tickets = set(get_users_viewable_tickets(request))
 
     # combine and sort the two types of posts
     posts = sorted(
